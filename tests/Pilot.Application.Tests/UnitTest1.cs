e0256ec8acf9a0867895af6a405aa4f90309049c
xexubonete <xexubonete@gmail.com>
2025-06-12 16:19:34 +0200

    Adding test projects

---
+++ tests/Pilot.Application.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Pilot.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/Pilot.Infrastructure.Test/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Pilot.Infrastructure.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
